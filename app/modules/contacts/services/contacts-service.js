91610822393186216963216c6ee7099b47db573d
Budmore <j.mach@budmore.pl>
2015-05-20 23:24:26 +0200

    contactsModule: service + resource

--- app/modules/contacts/services/contacts-service.js
+++ app/modules/contacts/services/contacts-service.js
@@ -4,8 +4,10 @@ angular
 	.module('contactsModule')
 	.service('contactsService', ['$q', 'contactsResource', function ($q, contactsResource) {
 
-		//var self = this;
-
+		/**
+		 * Get all contacts
+		 * @return {object} dfd Promise
+		 */
 		this.getContacts = function() {
 			var dfd = $q.defer();
 
@@ -32,7 +34,7 @@ angular
 				id: contactId
 			};
 
-			contactsResource.getContacts( params,
+			contactsResource.getContacts(params,
 				function getContactByIdSuccess(data) {
 					dfd.resolve(data);
 				}, function getContactByIdError(error) {
@@ -43,6 +45,62 @@ angular
 			return dfd.promise;
 		};
 
+		/**
+		 * Create contact
+		 * @param  {object} contact
+		 * @return {object} dfd Promise
+		 */
+		this.createContact = function(contact) {
+			var dfd = $q.defer();
 
+			contactsResource.createContact(contact,
+				function createContactSuccess(data) {
+					dfd.resolve(data);
+				}, function createContactError(error) {
+					dfd.reject(error);
+				}
+			);
+
+			return dfd.promise;
+		};
+
+		/**
+		 * Update contact
+		 * @param  {object} contact
+		 * @return {object} dfd Promise
+		 */
+		this.updateContact = function(contact) {
+			var dfd = $q.defer();
+
+			contactsResource.updateContact(contact,
+				function createContactSuccess(data) {
+					dfd.resolve(data);
+				}, function createContactError(error) {
+					dfd.reject(error);
+				}
+			);
+
+			return dfd.promise;
+		};
+
+
+		/**
+		 * Update contact
+		 * @param  {object} contact
+		 * @return {object} dfd Promise
+		 */
+		this.removeContact = function(contact) {
+			var dfd = $q.defer();
+
+			contactsResource.removeContact(null, contact,
+				function createContactSuccess(data) {
+					dfd.resolve(data);
+				}, function createContactError(error) {
+					dfd.reject(error);
+				}
+			);
+
+			return dfd.promise;
+		};
 
 	}]);
--- test/spec/modules/contacts/services/contacts-service.spec.js
+++ test/spec/modules/contacts/services/contacts-service.spec.js
@@ -17,6 +17,10 @@ describe('contactsModule services:', function() {
 		errorCb = jasmine.createSpy();
 	}));
 
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
+	});
 
 	describe('"contactsService"', function() {
 
@@ -34,16 +38,74 @@ describe('contactsModule services:', function() {
 		});
 
 		it('should GET contacts by id', function() {
-			$httpBackend.expectGET(globalSettings.baseUrl + '/contacts').respond(200, {});
-			contactsService.getContacts().then(successCb, errorCb);
+			var mocked = {
+				id: '123sad123'
+			};
+
+			$httpBackend.expectGET(globalSettings.baseUrl + '/contacts/' + mocked.id).respond(200, {});
+			contactsService.getContactById(mocked.id).then(successCb, errorCb);
 			$httpBackend.flush();
 			expect(successCb).toHaveBeenCalled();
 
-			$httpBackend.expectGET(globalSettings.baseUrl + '/contacts').respond(404, {});
-			contactsService.getContacts().then(successCb, errorCb);
+			$httpBackend.expectGET(globalSettings.baseUrl + '/contacts/' + mocked.id).respond(404, {});
+			contactsService.getContactById(mocked.id).then(successCb, errorCb);
 			$httpBackend.flush();
 			expect(errorCb).toHaveBeenCalled();
 		});
+
+		it('should POST create contact', function() {
+			var mocked = {
+				firstName: 'Jakub',
+				lastName: 'Mach'
+			};
+
+			$httpBackend.expectPOST(globalSettings.baseUrl + '/contacts').respond(200, {});
+			contactsService.createContact(mocked).then(successCb, errorCb);
+			$httpBackend.flush();
+			expect(successCb).toHaveBeenCalled();
+
+			$httpBackend.expectPOST(globalSettings.baseUrl + '/contacts').respond(404, {});
+			contactsService.createContact(mocked).then(successCb, errorCb);
+			$httpBackend.flush();
+			expect(errorCb).toHaveBeenCalled();
+		});
+
+		it('should PUT update contact', function() {
+			var mocked = {
+				id: 'as12ba',
+				firstName: 'Jakub',
+				lastName: 'Mach'
+			};
+
+			$httpBackend.expectPUT(globalSettings.baseUrl + '/contacts/' + mocked.id).respond(200, {});
+			contactsService.updateContact(mocked).then(successCb, errorCb);
+			$httpBackend.flush();
+			expect(successCb).toHaveBeenCalled();
+
+			$httpBackend.expectPUT(globalSettings.baseUrl + '/contacts/' + mocked.id).respond(404, {});
+			contactsService.updateContact(mocked).then(successCb, errorCb);
+			$httpBackend.flush();
+			expect(errorCb).toHaveBeenCalled();
+		});
+
+		it('should DELETE remove contact', function() {
+			var mocked = {
+				id: 'as12ba',
+				firstName: 'Jakub',
+				lastName: 'Mach'
+			};
+
+			$httpBackend.expectDELETE(globalSettings.baseUrl + '/contacts/' + mocked.id).respond(200, {});
+			contactsService.removeContact(mocked).then(successCb, errorCb);
+			$httpBackend.flush();
+			expect(successCb).toHaveBeenCalled();
+
+			$httpBackend.expectDELETE(globalSettings.baseUrl + '/contacts/' + mocked.id).respond(404, {});
+			contactsService.removeContact(mocked).then(successCb, errorCb);
+			$httpBackend.flush();
+			expect(errorCb).toHaveBeenCalled();
+		});
+
 
 	});
 });
