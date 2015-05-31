b1649707e6a1520be9b045505a1a03daa100e159
Budmore <j.mach@budmore.pl>
2015-06-01 00:59:45 +0200

    Module General: update /settings page

--- app/modules/general/controllers/settings-ctrl.js
+++ app/modules/general/controllers/settings-ctrl.js
@@ -4,7 +4,47 @@ angular.module('generalModule')
 	function ($scope) {
 		'use strict';
 
-		$scope.user = {};
+
+		$scope.user = {
+			email: 'j.mach@budmore.pl',
+			phone: 501502503,
+			emailNotifications: [
+				'j.mach@budmore.pl',
+				'jakub@budmore.pl',
+				'j.mach@gaminglive.tv',
+			],
+			notifications: {
+				daily: false,
+				weekly: true
+			}
+
+		};
+		var userCopy = angular.copy($scope.user);
+
+
+		$scope.revert = function() {
+			var temp = angular.copy(userCopy);
+			angular.extend($scope.user, temp);
+		}
+
+
+
+		$scope.saveUser = function(user) {
+			user.emailNotifications = validEmails(user.emailNotifications);
+
+			userCopy = angular.copy($scope.user);
+		}
+
+		function validEmails(emails) {
+			var result;
+			var regEx = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+			result = emails.filter(function(email) {
+				return regEx.test(email);
+			});
+
+			return result
+		}
 
 	}]
 );
